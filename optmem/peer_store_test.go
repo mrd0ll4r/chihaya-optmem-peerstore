27803cc706ef4f6eda016ab052a9d16c700bcce6
Leo Balduf <balduf@hm.edu>
2016-07-13 20:55:27 -0400

    tests: add more tests

--- optmem/peer_store_test.go
+++ optmem/peer_store_test.go
@@ -5,7 +5,11 @@ import (
 
 	"time"
 
+	"net"
+
+	"github.com/chihaya/chihaya"
 	"github.com/chihaya/chihaya/server/store"
+	"github.com/stretchr/testify/require"
 )
 
 var (
@@ -14,8 +18,206 @@ var (
 	peerStoreTestConfig  = &store.DriverConfig{Config: &peerStoreConfig{ShardCountBits: 10, GCInterval: time.Duration(1000000000), GCCutoff: time.Duration(1000000000)}}
 )
 
+var (
+	ih = chihaya.InfoHashFromString("00000000000000000000")
+	p1 = chihaya.Peer{
+		IP:   net.ParseIP("1.2.3.4"),
+		Port: 1234,
+	}
+	p2 = chihaya.Peer{
+		IP:   net.ParseIP("2.3.4.5"),
+		Port: 2345,
+	}
+)
+
 func init() {
 	peerStoreTester.CompareEndpoints()
+}
+
+func TestPutNumGetSeeder(t *testing.T) {
+	ps, err := (&peerStoreDriver{}).New(peerStoreTestConfig)
+	require.Nil(t, err)
+	require.NotNil(t, ps)
+
+	err = ps.PutSeeder(ih, p1)
+	require.Nil(t, err)
+
+	require.Equal(t, 1, ps.NumSeeders(ih))
+
+	seeders4, _, err := ps.GetSeeders(ih)
+	require.Nil(t, err)
+	require.NotNil(t, seeders4)
+
+	require.Equal(t, 1, len(seeders4))
+	require.Equal(t, p1.Port, seeders4[0].Port)
+	require.True(t, p1.IP.Equal(seeders4[0].IP))
+
+	leechers4, leechers6, err := ps.GetLeechers(ih)
+	require.Nil(t, err)
+	if leechers4 != nil {
+		require.Equal(t, 0, len(leechers4))
+	}
+	if leechers6 != nil {
+		require.Equal(t, 0, len(leechers6))
+	}
+
+	e := ps.Stop()
+	err = <-e
+	require.Nil(t, err)
+}
+
+func TestPutNumGetLeecher(t *testing.T) {
+	ps, err := (&peerStoreDriver{}).New(peerStoreTestConfig)
+	require.Nil(t, err)
+	require.NotNil(t, ps)
+
+	err = ps.PutLeecher(ih, p1)
+	require.Nil(t, err)
+
+	require.Equal(t, 1, ps.NumLeechers(ih))
+
+	leechers4, _, err := ps.GetLeechers(ih)
+	require.Nil(t, err)
+	require.NotNil(t, leechers4)
+
+	require.Equal(t, 1, len(leechers4))
+	require.Equal(t, p1.Port, leechers4[0].Port)
+	require.True(t, p1.IP.Equal(leechers4[0].IP))
+
+	seeders4, seeders6, err := ps.GetSeeders(ih)
+	require.Nil(t, err)
+	if seeders4 != nil {
+		require.Equal(t, 0, len(seeders4))
+	}
+	if seeders6 != nil {
+		require.Equal(t, 0, len(seeders6))
+	}
+
+	e := ps.Stop()
+	err = <-e
+	require.Nil(t, err)
+}
+
+func TestDeleteSeeder(t *testing.T) {
+	ps, err := (&peerStoreDriver{}).New(peerStoreTestConfig)
+	require.Nil(t, err)
+	require.NotNil(t, ps)
+
+	err = ps.PutSeeder(ih, p1)
+	require.Nil(t, err)
+
+	err = ps.PutSeeder(ih, p2)
+	require.Nil(t, err)
+
+	require.Equal(t, 2, ps.NumSeeders(ih))
+
+	err = ps.DeleteSeeder(ih, p2)
+	require.Nil(t, err)
+
+	require.Equal(t, 1, ps.NumSeeders(ih))
+
+	seeders4, _, err := ps.GetSeeders(ih)
+	require.Nil(t, err)
+	require.NotNil(t, seeders4)
+
+	require.Equal(t, 1, len(seeders4))
+	require.Equal(t, p1.Port, seeders4[0].Port)
+	require.True(t, p1.IP.Equal(seeders4[0].IP))
+
+	leechers4, leechers6, err := ps.GetLeechers(ih)
+	require.Nil(t, err)
+	if leechers4 != nil {
+		require.Equal(t, 0, len(leechers4))
+	}
+	if leechers6 != nil {
+		require.Equal(t, 0, len(leechers6))
+	}
+
+	e := ps.Stop()
+	err = <-e
+	require.Nil(t, err)
+}
+
+func TestDeleteLastSeeder(t *testing.T) {
+	ps, err := (&peerStoreDriver{}).New(peerStoreTestConfig)
+	require.Nil(t, err)
+	require.NotNil(t, ps)
+
+	err = ps.PutSeeder(ih, p1)
+	require.Nil(t, err)
+
+	err = ps.DeleteSeeder(ih, p1)
+	require.Nil(t, err)
+
+	require.Equal(t, 0, ps.NumSeeders(ih))
+
+	_, _, err = ps.GetSeeders(ih)
+	require.Equal(t, store.ErrResourceDoesNotExist, err)
+
+	e := ps.Stop()
+	err = <-e
+	require.Nil(t, err)
+}
+
+func TestDeleteLeecher(t *testing.T) {
+	ps, err := (&peerStoreDriver{}).New(peerStoreTestConfig)
+	require.Nil(t, err)
+	require.NotNil(t, ps)
+
+	err = ps.PutLeecher(ih, p1)
+	require.Nil(t, err)
+
+	err = ps.PutLeecher(ih, p2)
+	require.Nil(t, err)
+
+	require.Equal(t, 2, ps.NumLeechers(ih))
+
+	err = ps.DeleteLeecher(ih, p2)
+	require.Nil(t, err)
+
+	require.Equal(t, 1, ps.NumLeechers(ih))
+
+	leechers4, _, err := ps.GetLeechers(ih)
+	require.Nil(t, err)
+	require.NotNil(t, leechers4)
+
+	require.Equal(t, 1, len(leechers4))
+	require.Equal(t, p1.Port, leechers4[0].Port)
+	require.True(t, p1.IP.Equal(leechers4[0].IP))
+
+	seeders4, seeders6, err := ps.GetSeeders(ih)
+	require.Nil(t, err)
+	if seeders4 != nil {
+		require.Equal(t, 0, len(seeders4))
+	}
+	if seeders6 != nil {
+		require.Equal(t, 0, len(seeders6))
+	}
+
+	e := ps.Stop()
+	err = <-e
+	require.Nil(t, err)
+}
+
+func TestDeleteLastLeecher(t *testing.T) {
+	ps, err := (&peerStoreDriver{}).New(peerStoreTestConfig)
+	require.Nil(t, err)
+	require.NotNil(t, ps)
+
+	err = ps.PutLeecher(ih, p1)
+	require.Nil(t, err)
+
+	err = ps.DeleteLeecher(ih, p1)
+	require.Nil(t, err)
+
+	require.Equal(t, 0, ps.NumLeechers(ih))
+
+	_, _, err = ps.GetLeechers(ih)
+	require.Equal(t, store.ErrResourceDoesNotExist, err)
+
+	e := ps.Stop()
+	err = <-e
+	require.Nil(t, err)
 }
 
 func TestPeerStore(t *testing.T) {
